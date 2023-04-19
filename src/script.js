f7540d34466c25564fb612f5f74051cce0c51f70
ZhannaMelnyk <58272440+ZhannaMelnyk@users.noreply.github.com>
2023-04-19 12:36:13 +0300

    Added dark mode

--- src/script.js
+++ src/script.js
@@ -1,5 +1,3 @@
-const cardsContainer = document.getElementById('cards-container');
-
 const cardsAmountForLoading = 9;
 
 let currentPage = 1;
@@ -9,7 +7,7 @@ const getRandomInt = (max) => {
 };
 
 function* generateCardDescText() {
-	const text = "Pellentesque vitae velit augue. Donec lobortis interdum augue, et commodo mi posuere at. Etiam quis gravida risus, in venenatis arcu. Aenean ac consequat lorem, et suscipit dui. Integer vehicula massa vel lacus varius, sed rutrum massa porta. Aenean vitae mollis nibh. Vestibulum auctor sapien.";
+	const text = 'Pellentesque vitae velit augue. Donec lobortis interdum augue, et commodo mi posuere at. Etiam quis gravida risus, in venenatis arcu. Aenean ac consequat lorem, et suscipit dui. Integer vehicula massa vel lacus varius, sed rutrum massa porta. Aenean vitae mollis nibh. Vestibulum auctor sapien.';
 
 	yield text.slice(0, getRandomInt(294));
 };
@@ -19,41 +17,43 @@ const getCardDescText = () => {
 };
 
 const createCardView = (data) => {
-	const image = document.createElement("img");
-	image.className = "card-img-top";
-	image.alt = "card-image";
+	const cardsContainer = document.getElementById('cards-container');
+
+	const image = document.createElement('img');
+	image.className = 'card-img-top';
+	image.alt = 'card-image';
 	image.src = data.download_url
 
-	const title = document.createElement("h4");
-	title.className = "card-title fw-bold";
+	const title = document.createElement('h4');
+	title.className = 'card-title fw-bold';
 
-	const desc = document.createElement("p");
+	const desc = document.createElement('p');
 	desc.id = `desc-${data.id}`;
-	desc.className = "text-truncate text-truncate-custom mb-2";
+	desc.className = 'text-truncate text-truncate-custom mb-2';
 	desc.innerText = getCardDescText();
 
-	const cardBody = document.createElement("div");
+	const cardBody = document.createElement('div');
 	cardBody.id = `card-body-${data.id}`;
-	cardBody.className = "card-body";
+	cardBody.className = 'card-body';
 
-	const saveBtn = document.createElement("a");
-	saveBtn.className = "btn btn-primary text-white fw-bold me-3 mt-1";
-	saveBtn.href = "#";
-	saveBtn.innerText = "Save to collection";
+	const saveBtn = document.createElement('a');
+	saveBtn.className = 'btn btn-primary text-white fw-bold me-3 mt-1';
+	saveBtn.href = '#';
+	saveBtn.innerText = 'Save to collection';
 
-	const shareBtn = document.createElement("a");
-	shareBtn.className = "btn btn-outline-dark fw-bold border-grey border-middle-width share-btn mt-1";
-	shareBtn.href = "#";
-	shareBtn.innerText = "Share";
+	const shareBtn = document.createElement('a');
+	shareBtn.className = 'btn btn-outline-dark fw-bold border-grey border-middle-width share-btn mt-1';
+	shareBtn.href = '#';
+	shareBtn.innerText = 'Share';
 
-	const cardFooter = document.createElement("div");
-	cardFooter.className = "card-footer bg-white border-grey pt-1";
+	const cardFooter = document.createElement('div');
+	cardFooter.className = 'card-footer bg-white border-grey pt-1';
 
-	const card = document.createElement("div");
-	card.className = "card border-grey";
+	const card = document.createElement('div');
+	card.className = 'card border-grey';
 
-	const container = document.createElement("section");
-	container.className = "col";
+	const container = document.createElement('section');
+	container.className = 'col';
 	container.id = `card-${data.id}`
 
 	cardBody.appendChild(title);
@@ -72,10 +72,10 @@ const createCardView = (data) => {
 };
 
 const createShowBtn = (id, label) => {
-	const showBtn = document.createElement("button");
+	const showBtn = document.createElement('button');
 
 	showBtn.id = id;
-	showBtn.className = "btn bg-white text-dark p-0 lh-1 show-btn";
+	showBtn.className = 'btn bg-white text-dark p-0 lh-1 show-btn';
 	showBtn.innerText = label;
 
 	return showBtn;
@@ -92,7 +92,7 @@ const handleDescCollapsing = ({ body, desc, shouldShowingShowMore }) => {
 
 		desc.style.webkitLineClamp = 2;
 
-		const showMoreBtn = createShowBtn(`show-more-${desc.id}`, "Show more...");
+		const showMoreBtn = createShowBtn(`show-more-${desc.id}`, 'Show more...');
 		showMoreBtn.addEventListener('click', () => handleDescCollapsing({ body, desc, shouldShowingShowMore: false }));
 
 		body.appendChild(showMoreBtn);
@@ -103,7 +103,7 @@ const handleDescCollapsing = ({ body, desc, shouldShowingShowMore }) => {
 
 		desc.style.webkitLineClamp = 10;
 
-		const showLessBtn = createShowBtn(`show-less-${desc.id}`, "Show less...");
+		const showLessBtn = createShowBtn(`show-less-${desc.id}`, 'Show less...');
 		showLessBtn.addEventListener('click', () => handleDescCollapsing({ body, desc, shouldShowingShowMore: true }));
 
 		body.appendChild(showLessBtn);
@@ -112,13 +112,13 @@ const handleDescCollapsing = ({ body, desc, shouldShowingShowMore }) => {
 
 const collapseDesc = ({ body, desc, btn }) => {
 	if (desc.scrollHeight / 24 > 2) {
-		desc.className = "text-truncate text-truncate-custom mb-2";
+		desc.className = 'text-truncate text-truncate-custom mb-2';
 		
 		if (!btn) {
 			handleDescCollapsing({ body, desc, shouldShowingShowMore: true });
 		};
 	} else if (btn && desc.offsetHeight / 24 <= 2) {
-		desc.className = "mb-2";
+		desc.className = 'mb-2';
 		
 		btn.remove();
 	};
@@ -186,11 +186,30 @@ const debounce = (func, timeout = 100) => {
 	};
 };
 
-const handlePageResize = debounce(() => onPageResize());
+const handleClickDarkMode = (event) => {
+	const darkModeSwitch = event.target;
 
-window.onload = () => {
-	addNewCards(currentPage);
-};
+	if (darkModeSwitch.getAttribute('aria-checked') === 'true') {
+		darkModeSwitch.setAttribute('aria-checked', 'false');
+		document.documentElement.setAttribute('data-bs-theme', 'light')
+	} else {
+		darkModeSwitch.setAttribute('aria-checked', 'true');
+		document.documentElement.setAttribute('data-bs-theme', 'dark')
+	}
+}
 
-window.addEventListener("scroll", handleInfiniteScroll);
-window.addEventListener("resize", handlePageResize);
+const renderAndAddEventsToPage = () => {
+	const handlePageResize = debounce(() => onPageResize());
+
+	const darkModeSwitch = document.getElementById('dark-mode-switch');
+	darkModeSwitch.addEventListener('click', handleClickDarkMode)
+
+	window.onload = () => {
+		addNewCards(currentPage);
+	};
+
+	window.addEventListener('scroll', handleInfiniteScroll);
+	window.addEventListener('resize', handlePageResize);
+}
+
+renderAndAddEventsToPage();
